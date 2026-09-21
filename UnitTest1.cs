ee808d93b12bf795d367be293fa5f2ca9337c9e2
Galymzhankyzy Aknur <251912@astanait.edu.kz>
2026-09-22 02:25:24 +0600

    этот тоже 1 таск 4 чаптера 3-я часть

---
+++ UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Ch04Ex02PrimeFactorsLib;
+namespace Ch04Ex02PrimeFactorsTests;
+public class UnitTest1
+{
+    [Fact]
+    public void Test4()
+    {
+        Assert.Equal("2 x 2", PrimeFactorsHelper.PrimeFactors(4));
+    }
+
+    [Fact]
+    public void Test7()
+    {
+        Assert.Equal("7", PrimeFactorsHelper.PrimeFactors(7));
+    }
+
+    [Fact]
+    public void Test30()
+    {
+        Assert.Equal("2 x 3 x 5", PrimeFactorsHelper.PrimeFactors(30));
+    }
+
+    [Fact]
+    public void Test40()
+    {
+        Assert.Equal("2 x 2 x 2 x 5", PrimeFactorsHelper.PrimeFactors(40));
+    }
+
+    [Fact]
+    public void Test50()
+    {
+        Assert.Equal("2 x 5 x 5", PrimeFactorsHelper.PrimeFactors(50));
+    }
+}
