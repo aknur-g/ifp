85195883b97ae3d0b1a6ac5ee1d9a7034fcd9f0b
Galymzhankyzy Aknur <251912@astanait.edu.kz>
2026-09-22 02:16:54 +0600

    loops and overflow 3chapter 1 book 1task

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+int max = 500;
+
+for (byte i = 0; i < max; i++)
+{
+if (i == byte.MaxValue)
+{
+    WriteLine("WARNING: i is about to overflow.");
+    break;
+}
+}
