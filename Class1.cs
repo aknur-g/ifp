470616de0557ee363b4ce5967b6f15a157e9f5db
Galymzhankyzy Aknur <251912@astanait.edu.kz>
2026-09-22 02:24:36 +0600

    кстати в 4 чаптере все 3 кода это 1 таск
    то есть в одну папку пихаешь все 3 кода

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace Ch04Ex02PrimeFactorsLib;
+
+public static class PrimeFactorsHelper
+{
+    public static string PrimeFactors(int number)
+    {
+        List<int> factors = new List<int>();
+
+        for (int factor = 2; factor <= number; factor++)
+        {
+            while (number % factor == 0)
+            {
+                factors.Add(factor);
+                number /= factor;
+            }
+        }
+
+        return string.Join(" x ", factors);
+    }
+}
